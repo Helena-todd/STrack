fe36c7397d8519fbd0c4f8a963ef6213793c9928
Helena-todd <helena_t@hotmail.fr>
2023-02-17 18:03:17 +0100

    Add files via upload
    Uploaded version of STrack: allows interactive use in docker Desktop and exports a complete table containing results of STrack for all timepoints

--- Docker_structure/job_call_main_docker.sh
+++ Docker_structure/job_call_main_docker.sh
@@ -2,17 +2,25 @@
 #                       ${1}                 ${2}     ${3}
 
 # Move to the folder containing the Raw images
-cd ${1}/exported/
+cd /home/test_images/
 
 ######################################
 #####    STrack cell tracking    #####
 ######################################
 
-echo "Tracking cells using STrack "
+echo "Tracking cells using STrack"
 
 # Create folder for STrack outputs
-mkdir -p ${1}/STrack/
-strack_dir="${1}/STrack/"
+mkdir -p /home/test_images/STrack/
+strack_dir="/home/test_images/STrack/"
+
+MAXDIST=${2}
+MAXANGLE=${3}
+export MAXDIST
+export MAXANGLE
 
 # Launch script to extract features
-python3 /home/scripts/strack_script_v4.py ${2} ${3} ${strack_dir}
+python3 /home/scripts/strack_script_v4.py $MAXDIST $MAXANGLE ${strack_dir}
+
+# Launch script to merge all STrack result tables into one csv file
+python3 -W ignore /home/scripts/strack_merge_tables.py ${strack_dir}
